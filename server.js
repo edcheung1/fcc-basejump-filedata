f2fabf8cac8fc9db7b72032ae94daa145b90d311
Ed <edcheung@umich.edu>
2016-01-28 19:10:11 +0000

    changed to use 'route'

--- server.js
+++ server.js
@@ -13,21 +13,33 @@ app.use(bodyParser.json());
 app.set('port', (process.env.PORT || 5000));
 app.set('views', path.join(__dirname, 'views'));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/public/index.html');
-});
+app.route('/')
+  .get(function(req, res) {
+    res.sendFile(process.cwd() + '/public/index.html');
+  })
+  .post(multer({ dest: './uploads/'}).single('upl'), function(req,res) {
+    console.log(req.body);
+    
+    console.log(req.file);
+    
+    var fileSize = req.file.size;
+    
+    fs.unlinkSync('./uploads/' + req.file.filename);
+    
+    res.send(JSON.stringify({'file-size': fileSize}));
+  });
 
-app.post('/', multer({ dest: './uploads/'}).single('upl'), function(req,res) {
-  console.log(req.body);
+// app.post('/', multer({ dest: './uploads/'}).single('upl'), function(req,res) {
+//   console.log(req.body);
   
-  console.log(req.file);
+//   console.log(req.file);
   
-  var fileSize = req.file.size;
+//   var fileSize = req.file.size;
   
-  fs.unlinkSync('./uploads/' + req.file.filename);
+//   fs.unlinkSync('./uploads/' + req.file.filename);
   
-  res.send(JSON.stringify({'file-size': fileSize}));
-});
+//   res.send(JSON.stringify({'file-size': fileSize}));
+// });
 
 app.listen(app.get('port'), function () {
         console.log('Node app is running on port ', app.get('port'));
